2aa00303957d5d2c8af64bf7146150e8cb78062c
PratikCods <sandyhammer31@gmail.com>
2020-08-15 23:41:06 +0530

    added password recovery system

--- my_blog/views.py
+++ my_blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
-from django.urls import reverse
+from django.urls import reverse,reverse_lazy
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from . models import Post
 
@@ -9,9 +10,18 @@ class PostListView(generic.ListView):
     model = Post
     context_object_name = 'posts'
     template_name = "my_blog/home.html"
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(generic.ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = "my_blog/user_posts.html"
+    paginate_by = 5
 
     def get_queryset(self):
-        return Post.objects.order_by('-date_posted')
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
     
 class PostDetailView(generic.DetailView):
     model = Post
@@ -36,10 +46,10 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,generic.edit.UpdateV
     def test_func(self):
         return self.get_object().author == self.request.user
 
-class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,generic.DetailView):
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,generic.edit.DeleteView):
     model = Post
-    success_url = "/"
-    template_name_suffix = "_delete"
+    success_url = reverse_lazy('blog-home')
+    template_name = "my_blog/post_delete.html"
     context_object_name = "post"
 
     def test_func(self):
